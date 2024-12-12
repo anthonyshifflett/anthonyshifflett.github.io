3cda1c9224f0e3e61263060c7932d91ae9e27855
Anthony Shifflett <anthonydshifflett@gmail.com>
2024-12-12 05:38:00 -0500

    Update build configurations

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
